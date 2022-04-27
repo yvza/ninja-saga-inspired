d6e201bfb095749a5c6a934a235d0a961af2c268
yuza <akhmadyuza@gmail.com>
2022-04-27 13:54:23 +0700

    refactor

--- enemyTurn.php
+++ enemyTurn.php
@@ -1,14 +1,16 @@
 <?php
 function enemyTurn($action, $player, $enemy) {
+  $randomValue = rand(20, 40);
+
   switch($action) {
     case 1: //attack
-      $randomBaseAttack = rand(20, 40);
+      $randomBaseAttack = $randomValue;
       echo "Enemy hit you -".$randomBaseAttack."\n";
       $player->attack($randomBaseAttack);
     break;
     
     case 2: //heal
-      $randomBaseHeal = rand(20, 40);
+      $randomBaseHeal = $randomValue;
       echo "Enemy Healing +".$randomBaseHeal."\n";
       $enemy->heal($randomBaseHeal);
     break;
